fe8bfad69c591b505cab518c5862733897c1de5e
Jannik Renfordt <jannik.renfordt@ikobb.de>
2024-05-18 15:18:28 +0200

    Add data provider for HexColor::toRGB() test
    A new data provider, hexToRgbProvider(), has been added to the HexColorTest unit test class. This will provide test cases for the HexColor::toRGB() method. The method `test_invalid_hex_string_throws_exception` has been replaced with `test_toRGB` that uses the data provider for multiple test cases and `test_toRGB_with_invalid_hex` to handle invalid hex cases.

--- tests/HexColorTest.php
+++ tests/HexColorTest.php
@@ -11,6 +11,19 @@ use InvalidArgumentException;
  */
 class HexColorTest extends TestCase
 {
+    /**
+     * Provides data for testing the HexColor::toRGB() method.
+     */
+    public function hexToRgbProvider(): array
+    {
+        return [
+            'black' => ['#ffffff', [255, 255, 255]],
+            'white' => ['#000000', [0, 0, 0]],
+            'Mountain Meadow' => ['#11c380', [17, 195, 128]],
+            'Sienna' => ['#8c5a45', [140, 90, 69]],
+            'Dark Slate Grey' => ['#345', [51, 68, 85]]
+        ];
+    }
 
     /**
      * @covers \Renfordt\Colors\HexColor::setHexStr
@@ -20,27 +33,41 @@ class HexColorTest extends TestCase
         $color = new HexColor();
         $color->setHexStr('#123abc');
         $this->assertSame('123abc', $color->getHexStr(false));
+
+        $color->setHexStr('#abc');
+        $this->assertSame('aabbcc', $color->getHexStr(false));
     }
 
+
     /**
-     * @covers \Renfordt\Colors\HexColor::setHexStr
+     * @covers       \Renfordt\Colors\HexColor::toRGB
+     * @dataProvider hexToRgbProvider
      */
-    public function test_invalid_hex_string_throws_exception()
+    public function test_toRGB($hex, $expected): void
     {
         $color = new HexColor();
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('The format of the hex is invalid.');
-        $color->setHexStr('1234');
+        $color->setHexStr($hex);
+        $this->assertSame($expected, $color->toRGB());
+
     }
 
-    /**
-     * @covers \Renfordt\Colors\HexColor::setHexStr
-     * @covers \Renfordt\Colors\HexColor::getHexStr
-     */
     public function test_withHash_returns_hashed_hex_string()
     {
         $color = new HexColor();
         $color->setHexStr('#fff');
         $this->assertSame('#fff', $color->getHexStr(true));
+    }
+
+
+    /**
+     * @covers \Renfordt\Colors\HexColor::toRGB
+     */
+    public function test_toRGB_with_invalid_hex()
+    {
+        $color = new HexColor();
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The format of the hex is invalid.');
+        $color->setHexStr('ghijk');
+        $color->toRGB();
     }
 }
