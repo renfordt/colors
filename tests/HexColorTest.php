7e1daeb5d1d35355462ebb3078e610b0eb3d7128
Jannik Renfordt <jannik.renfordt@ikobb.de>
2024-05-18 15:47:22 +0200

    Update HexColorTest to use RGBColor class
    The HexColorTest was modified to utilize the RGBColor::make method in its data provider ("hexToRgbProvider"). This change ensures that the test's expected results match the output of the actual RGBColor class. Additionally, a new test case was added to validate the HexColor::toRGB method against invalid hexadecimal lengths.

--- tests/HexColorTest.php
+++ tests/HexColorTest.php
@@ -5,6 +5,7 @@ namespace Renfordt\Colors\Tests;
 use PHPUnit\Framework\TestCase;
 use Renfordt\Colors\HexColor;
 use InvalidArgumentException;
+use Renfordt\Colors\RGBColor;
 
 /**
  * @covers \Renfordt\Colors\HexColor
@@ -17,11 +18,11 @@ class HexColorTest extends TestCase
     public static function hexToRgbProvider(): array
     {
         return [
-            'black' => ['#ffffff', [255, 255, 255]],
-            'white' => ['#000000', [0, 0, 0]],
-            'Mountain Meadow' => ['#11c380', [17, 195, 128]],
-            'Sienna' => ['#8c5a45', [140, 90, 69]],
-            'Dark Slate Grey' => ['#345', [51, 68, 85]]
+            'black' => ['#ffffff', RGBColor::make([255, 255, 255])],
+            'white' => ['#000000', RGBColor::make([0, 0, 0])],
+            'Mountain Meadow' => ['#11c380', RGBColor::make([17, 195, 128])],
+            'Sienna' => ['#8c5a45', RGBColor::make([140, 90, 69])],
+            'Dark Slate Grey' => ['#345', RGBColor::make([51, 68, 85])]
         ];
     }
 
@@ -68,6 +69,18 @@ class HexColorTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('The format of the hex is invalid.');
         $color->setHexStr('ghijk');
+        $color->toRGB();
+    }
+
+    /**
+     * @covers \Renfordt\Colors\HexColor::toRGB
+     */
+    public function test_toRGB_with_invalid_hex_length()
+    {
+        $color = new HexColor();
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Hex Str length must be 6 or 3 characters.');
+        $color->setHexStr('abcd');
         $color->toRGB();
     }
 }
