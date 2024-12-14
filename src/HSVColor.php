d1b5aca9e89dd8f1ca285d12586b7d42954b86be
renfordt <jannikrenfordt@gmx.de>
2024-12-14 21:21:36 +0100

    Update HSVColor type annotations for improved accuracy
    Added more specific type hints to HSV-related methods, replacing generic `array` types with `array{0:int, 1:float, 2:float}`. This improves code clarity and helps with static analysis tools by specifying the exact structure of the HSV arrays.

--- src/HSVColor.php
+++ src/HSVColor.php
@@ -18,7 +18,7 @@ class HSVColor
     /**
      * Creates a new instance of the HSVColor class based on provided HSV values.
      *
-     * @param  array  $hsv  An array containing the hue, saturation, and value components in that order.
+     * @param  array{0:int, 1:float, 2:float}  $hsv  An array containing the hue, saturation, and value components in that order.
      * @return HSVColor          The newly created HSVColor object.
      */
     public static function create(array $hsv): HSVColor
@@ -37,7 +37,7 @@ class HSVColor
      * This method initializes an HSVColor object using an array of HSV (Hue, Saturation, Value) values
      * and returns the created HSVColor instance.
      *
-     * @param  array  $hsv  The array of HSV values used to create the HSVColor.
+     * @param  array{0:int, 1:float, 2:float}  $hsv  The array of HSV values used to create the HSVColor.
      * @return HSVColor The created HSVColor instance.
      * @deprecated 1.0.1 Use ::create method
      */
@@ -145,7 +145,7 @@ class HSVColor
     /**
      * Retrieves the HSV color components.
      *
-     * @return array The HSV color components as an array, where:
+     * @return array{0:int, 1:float, 2:float} The HSV color components as an array, where:
      *               - The first element is the hue component.
      *               - The second element is the saturation component.
      *               - The third element is the value component.
